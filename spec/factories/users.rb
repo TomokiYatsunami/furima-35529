a1a4ab5dbe982b2624dbcd56ba201c1eba9dd363
TomokiYatsunami <t.yatsunami.business@gmail.com>
2021-05-20 18:26:21 +0900

    Faker使用完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
 
   factory :user do
-    nickname {"aaa"}
-    email {"aaa@gmail.com"}
-    password {"aaaa11"}
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name {"赤沙汰"}
     last_name_kana {"アカサタ"}
     first_name {"濱家ら"}
     first_name_kana {"ハマヤラ"}
-    birthday {"2020-02-02"}
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
   end
 
 end
