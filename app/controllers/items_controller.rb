cc1edeb7442a76c640a17a1c4b4c1c78ea7b8e03
TomokiYatsunami <t.yatsunami.business@gmail.com>
2021-05-23 11:11:10 +0900

    販売利益、純利益以外の記入欄部分実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
 
   def index
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :description, :price, :status, :ship_price, :ship_date, :category, :prefecture).merge(user_id: current_user.id)
+  end
 end
