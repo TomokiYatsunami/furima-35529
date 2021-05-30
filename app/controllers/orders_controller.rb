2ff292d6bbb3c48e0a388ac918bf672cd11b712a
TomokiYatsunami <t.yatsunami.business@gmail.com>
2021-05-30 21:07:58 +0900

    クレジットカード情報が正確に記載された場合payjpを通して支払われる

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @product = Product.find(params[:product_id])
     @ship_record = ShipRecord.new(ship_record_params)
     if @ship_record.valid?
+      pay_item
       @ship_record.save
       redirect_to root_path
     else
@@ -20,6 +21,15 @@ class OrdersController < ApplicationController
 
   def ship_record_params
     params.require(:ship_record).permit(:postal_code, :city, :address, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_ebc69ffbb802614481a30c7c"
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: ship_record_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
