d3950f64e1f796902b2cc016845ab11a2578ebe3
TomokiYatsunami <t.yatsunami.business@gmail.com>
2021-05-30 18:31:22 +0900

    クレカ決済以外の単体テストコード

---
+++ spec/factories/ship_records.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+
+  factory :ship_record do
+    postal_code {"123-4567"}
+    city {"札幌市"}
+    address {"青山1-1-1"}
+    building {"柳ビル103"}
+    phone_number {"09000000000"}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+  end
+  
+end
--- spec/models/ship_record_spec.rb
+++ spec/models/ship_record_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe Ship_record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ShipRecord, type: :model do
+  before do
+    @user = FactoryBot.create(:user)
+    @product = FactoryBot.create(:product)
+    @ship_record = FactoryBot.build(:ship_record, user_id: @user.id, product_id: @product.id)
+    sleep 1 #SQLの処理がspecの処理より遅いときに書く、この場合は1秒作業を遅らせれる。
+  end
+
+  describe '商品購入機能' do
+    context '商品の購入ができる時' do
+      
+      it "全ての項目の入力が存在すれば登録できる" do
+        expect(@ship_record).to be_valid
+      end
+
+    end
+
+    context '商品の購入ができない時' do
+
+      it 'postal_codeが空では登録できない' do
+        @ship_record.postal_code = ''
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "Postal code can't be blank"
+      end
+
+      it 'prefecture_idが1では登録できない' do
+        @product.prefecture_id = 1  
+        @product.valid?
+        expect(@product.errors.full_messages).to include "Prefecture must be other than 1"
+      end
+
+      it 'cityが空では登録できない' do
+        @ship_record.city = ''
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "City can't be blank"
+      end
+
+      it 'addressが空では登録できない' do
+        @ship_record.address = ''
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "Address can't be blank"
+      end
+
+      it 'phone_numberが空では登録できない' do
+        @ship_record.phone_number = ''
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "Phone number can't be blank"
+      end
+      
+      it '郵便番号にハイフンが無いと保存不可能であること' do
+        @ship_record.postal_code = "8987654"
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "Postal code is invalid"
+      end
+
+      it '電話番号は12桁以上の数は保存不可能なこと' do
+        @ship_record.phone_number = "012345678910"
+        @ship_record.valid?
+        expect(@ship_record.errors.full_messages).to include "Phone number is invalid"
+      end
+
+    end
+  end
 end
