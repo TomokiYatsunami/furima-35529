9baa16a0bc8268956e3bf22a191ed894fc81d545
TomokiYatsunami <t.yatsunami.business@gmail.com>
2021-05-31 10:25:59 +0900

    環境変数設定、heroku上も完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_ebc69ffbb802614481a30c7c"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @product.price,
       card: ship_record_params[:token],
